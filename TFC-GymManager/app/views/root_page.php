7a457d8d30f460692eaefe58ed2cdea19598b9e4
jlopez-02 <juanlopezpem@gmail.com>
2023-03-11 15:48:06 +0100

    netbeans

---
+++ TFC-GymManager/app/views/root_page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GymManager</title>
+        <link rel="stylesheet" href="../styles/root.css"/>
+    </head>
+    <body>
+        
+        <div>
+            <header>
+                <nav id="root_navbar">
+                    <h1>Hola</h1>
+                </nav>
+            </header>
+        </div>
+
+
+
+    </body>
+</html>
