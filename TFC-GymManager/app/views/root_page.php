630bb48d0d9e7c189b39975a07a5842f2d990418
jlopez-02 <juanlopezpem@gmail.com>
2023-03-12 16:56:37 +0100

    Root Page Style

--- TFC-GymManager/app/views/root_page.php
+++ TFC-GymManager/app/views/root_page.php
@@ -1,16 +1,52 @@
 <!DOCTYPE html>
 <html>
+    <configuration>
+        <system.webServer>
+            <caching enabled="false" enableKernelCache="false" /> <!-- This one -->
+        </system.webServer>
+    </configuration>
     <head>
         <meta charset="UTF-8">
         <title>GymManager</title>
-        <link rel="stylesheet" href="../styles/root.css"/>
+
+        <!--Styles-->
+        <link rel="stylesheet" href="../styles/css/reset.css"/> <!-- Reduce Browser inconsistencies-->
+        <link rel="stylesheet" href="../styles/css/root.css"/> <!-- Main CSS -->
     </head>
     <body>
-        
         <div>
-            <header>
-                <nav id="root_navbar">
-                    <h1>Hola</h1>
+            <header id="root_header">
+                <nav id="nav_container">
+                    <a href="#" class="app_logo">
+                        GymManager
+                    </a>
+
+                    <div id="nav_menu">
+                        <ul id="nav_list">
+                            <li class="nav_item">
+                                <a href="#" class="nav_link">Home</a>
+                            </li>
+                            <li class="nav_item">
+                                <a href="#" class="nav_link">Plans</a>
+                            </li>
+                            <li class="nav_item">
+                                <a href="#" class="nav_link">About</a>
+                            </li>
+                            <li class="nav_item">
+                                <a href="#" class="nav_link">Contact</a>
+                            </li>
+
+                            <div class="nav_button_container">
+                                <a href="#" class="nav_button">Login</a>
+                            </div>
+
+                            <div class="nav_button_container">
+                                <a href="#" class="nav_button">Register</a>
+                            </div>
+                            
+                        </ul>
+                    </div>
+
                 </nav>
             </header>
         </div>
